1c40482edf62f2b44a5fb5f2a77452370ed33ddb
ameer <daxiameer@gmail.com>
2026-05-11 23:56:10 +0300

    added order to favorite items

--- ui/multipage_app/pages/Favorite items.py
+++ ui/multipage_app/pages/Favorite items.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 
-from api import get_user_favorites, handle_favorites
+from api import get_user_favorites, add_to_cart, handle_favorites
 
 
 
@@ -20,14 +20,16 @@ if st.session_state.get("refresh_favorites"):
 st.session_state["favorites"] = pd.DataFrame(fetch_favorites())
 
 if not st.session_state["favorites"].empty:
-    col1,col2,col3 = st.columns([1,1,1])
+    col1,col2,col3,col4,col5,col6 = st.columns([1,1,1,1,1,1])
     with col1:
         st.write("Product name")
     with col2:
         st.write("Price")
+    with col6:
+        st.write("inventory")
 
     for _, row in st.session_state["favorites"].iterrows():
-        col1,col2,col3 = st.columns([1,1,1])
+        col1,col2,col3,col4,col5,col6 = st.columns([1,1,1,1,1,1])
         with col1:
             st.write(row["name"])
         with col2:
@@ -37,6 +39,24 @@ if not st.session_state["favorites"].empty:
                 handle_favorites(row["name"])
                 fetch_favorites.clear()
                 st.rerun()
+        with col4:
+            order_quantity= st.number_input(
+            "Amount", min_value=1, value=1, step=1,
+                key=f"qty_{row['name']}"
+            )
+        with col5:
+            if st.button("Order", key=f"order_{row['name']}"):
+                if row["inventory"]<order_quantity:
+                    st.error("Not enough products in inventory!")
+                else:
+                    response = add_to_cart(row["name"],order_quantity)
+                    if response.status_code == 201:
+                        st.success(f"Ordered {row['name']}!")
+                        st.session_state["refresh_orders"] = True
+                    elif response.status_code == 400:
+                        st.error(response.json()["detail"])
+        with col6:
+            st.write(str(row["inventory"]))
 else:
     st.subheader("No items in favorites")
 
